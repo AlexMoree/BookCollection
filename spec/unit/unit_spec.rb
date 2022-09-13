ac7eeae6ec610b319ba241a1876c799952f71341
AlexMoree <alex.d.moree@gmail.com>
2022-09-13 02:49:53 -0500

    finished integration testing

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,60 +3,30 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'J K Rowling', price: 14.99, published: '2020-09-28')
   end
 
-  it 'is valid with valid attributes' do
+  it 'is valid with all valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a title' do
     subject.title = nil
     expect(subject).not_to be_valid
-  end
-end
-  # for Author
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(author: 'J K Rowling')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
   end
 
   it 'is not valid without a author' do
     subject.author = nil
     expect(subject).not_to be_valid
   end
-end
-# for Price
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(price: '14.99')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
 
   it 'is not valid without a price' do
     subject.price = nil
     expect(subject).not_to be_valid
   end
-end
-# For Date
-RSpec.describe Book, type: :model do
-  subject do
-    described_class.new(published: '04/02/1998')
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
 
   it 'is not valid without a published date' do
     subject.published = nil
     expect(subject).not_to be_valid
-  end 
+  end
 end
